b1bdff6d896c028917f90abd9e5d621cc01b89de
RifdaRifdiana <119845284+RifdaRifdiana@users.noreply.github.com>
2023-01-16 23:39:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Rifda_Rifdiana</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="background-color: aquamarine;">
+<a href="index.html">home</a>
+<a href="https://rifdacantik.wordpress.com/">websiteku</a>
+<h1 style="background-color: antiquewhite;">NAMA: Rifda Rifdiana</h1>
+<h2 style="background-color: antiquewhite;">ALAMAT: jl. Tenggilis Kauman no.29 Surabaya</h2>
+<h3 style="background-color: antiquewhite;">TEMPAT TANGGAL LAHIR: Surabaya, 12 Agustus 2002</h3>
+<h4 style="background-color: antiquewhite;">JENIS KELAMIN: Perempuan</h4>
+<h5 style="background-color: antiquewhite;">STATUS: Mahasiswa</h5>
+<h6 style="background-color: antiquewhite;">NOMOR TELEPON: 085850983087</h6>
+<h7 style="background-color: antiquewhite;">EMAIL: rifdarifdiana53@gmail.com</h7>
+<p>Program Studi: Teknik Informatika.</p>
+<blockquote>Universitas Yudharta Pasuruan</blockquote>
+<img src="gambar/foto1.jpeg" alt="foto1.jpeg" width="200" height="300"></img>
+<img src="https://i.ibb.co/10FQnPf/foto2.jpg" alt="foto2" width="200" height="300"></img>
+
+</body>
+</html>
